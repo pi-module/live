4743f7ab9df62e8fa248d59563fa52518e616b84
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2020-05-09 13:12:53 +0430

    Add admin section

---
+++ src/Controller/Admin/IndexController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Pi Engine (http://piengine.org)
+ *
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+
+namespace Module\Live\Controller\Admin;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+class IndexController extends ActionController
+{
+    public function indexAction()
+    {
+        // Get info from url
+        $module = $this->params('module');
+
+        // Get config
+        $config = Pi::service('registry')->config->read($module);
+
+        // Set player
+        $player = Pi::api('stream', 'live')->player();
+
+        // Set view
+        $this->view()->setTemplate('live:front/stream');
+        $this->view()->assign('config', $config);
+        $this->view()->assign('player', $player);
+    }
+}
